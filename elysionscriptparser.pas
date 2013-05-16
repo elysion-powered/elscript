2c118c1724dbb8eacfab5b2f5bdcbd3bcd1681f4
Johannes Stein <johannesstein@freeze-dev.com>
2013-05-16 21:20:37 +0200

    Added script parser shell

---
+++ elysionscriptparser.pas
@@ -0,0 +1,42 @@
+unit ElysionScriptParser;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, ElysionScriptVariable;
+
+type
+
+  { TelScriptParser }
+
+  TelScriptParser = class
+  public
+    constructor Create;
+    destructor Destroy; Override;
+
+    function Parse(aString: WideString): Boolean;
+  end;
+
+implementation
+
+{ TelScriptParser }
+
+constructor TelScriptParser.Create;
+begin
+
+end;
+
+destructor TelScriptParser.Destroy;
+begin
+  inherited Destroy;
+end;
+
+function TelScriptParser.Parse(aString: WideString): Boolean;
+begin
+
+end;
+
+end.
+
